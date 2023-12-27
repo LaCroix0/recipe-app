84ed565d1594c814964e56719d4b982e2738ce28
Jakub Wiańczyk <jwianczyk@gmail.com>
2023-12-27 19:47:07 +0100

    Fixed linting

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -45,4 +45,3 @@ class TagViewSet(mixins.UpdateModelMixin,
     def get_queryset(self):
         """Filter queryset to authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
-
